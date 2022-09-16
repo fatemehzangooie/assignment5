1ddbef343b65e40da6ac6f1ddff10355188013ae
Fatemeh-Zangooie-roshnavand <110530784+fatemehzangooie@users.noreply.github.com>
2022-09-16 06:56:46 -0700

    Add files via upload

---
+++ palindorme.py
@@ -0,0 +1,17 @@
+text=input('write the word: ')
+list_text1=[]
+list_text2=[]
+
+for letter in text:
+    list_text1.append(letter)
+    
+while ... in list_text1:
+        list_text1.remove(...)
+        
+for i in range(len(list_text1)):
+           list_text2.append(list_text1[-i-1])
+           
+           if list_text1==list_text2:
+               print('it is palindorme')
+           else:
+               print("it is not a palindorme") 
